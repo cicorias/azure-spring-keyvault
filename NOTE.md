b16a7dc14dc14f2caf9d906388a6bd3579c14c80
Shawn Cicoria <github@cicoria.com>
2021-07-26 15:29:20 -0400

    updates

---
+++ NOTE.md
@@ -0,0 +1,4 @@
+
+
+https://docs.microsoft.com/en-us/azure/developer/java/spring-framework/configure-spring-boot-starter-java-app-with-azure-key-vault
+
--- src/main/java/com/cicoria/azurespringkeyvault/controllers/GetSecretController.java
+++ src/main/java/com/cicoria/azurespringkeyvault/controllers/GetSecretController.java
@@ -4,15 +4,20 @@ package com.cicoria.azurespringkeyvault.controllers;
 import com.azure.security.keyvault.secrets.SecretClient;
 import com.azure.security.keyvault.secrets.SecretClientBuilder;
 import com.azure.security.keyvault.secrets.models.KeyVaultSecret;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GetSecretController {
+
+    @Value("${foobar}")
+    private String mySecretProperty;
+
     @GetMapping("/")
     public String index(){
-        String secret = getSecret("foobar");
-        return "you might have a secret";
+
+        return "you might have a secret: " + mySecretProperty;
     }
 
     private String getSecret(String key){
